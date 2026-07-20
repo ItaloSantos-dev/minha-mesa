63c35f7613d4e15cc2df54a52643e5b19a9a9109
Italo Santos <italosantoscont@gmail.com>
2026-07-20 15:37:47 -0300

    first commit

---
+++ src/main/java/com/italosantos/minha_mesa/MinhaMesaApplication.java
@@ -0,0 +1,13 @@
+package com.italosantos.minha_mesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaMesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaMesaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/italosantos/minha_mesa/MinhaMesaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.italosantos.minha_mesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaMesaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
