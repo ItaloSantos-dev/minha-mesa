e5b249e009b1023adfa02323130c176b66ca17c1
Italo Santos <italosantoscont@gmail.com>
2026-07-24 17:15:22 -0300

    feat(table): create a function for desactive table

--- src/main/java/com/italosantos/minha_mesa/controller/TableController.java
+++ src/main/java/com/italosantos/minha_mesa/controller/TableController.java
@@ -8,10 +8,7 @@ import com.italosantos.minha_mesa.model.UserModel;
 import com.italosantos.minha_mesa.service.TableService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -30,5 +27,11 @@ public class TableController {
     public ResponseEntity<TableResponseDTO> createTable(@AuthenticationPrincipal UserModel userModel, @RequestBody CreateTableRequestDTO createTableRequestDTO){
         TableModel tableModel = this.tableService.createTable(createTableRequestDTO, userModel);
         return ResponseEntity.created(URI.create("/tables" + tableModel.getId())).body(this.tableMapper.modelToResponse(tableModel));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTableById(@AuthenticationPrincipal UserModel userModel, @PathVariable  Integer id){
+        this.tableService.deleteTableById(userModel, id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/italosantos/minha_mesa/service/TableService.java
+++ src/main/java/com/italosantos/minha_mesa/service/TableService.java
@@ -3,6 +3,7 @@ package com.italosantos.minha_mesa.service;
 import com.italosantos.minha_mesa.dto.table.CreateTableRequestDTO;
 import com.italosantos.minha_mesa.exception.AlreadyExistTableWithNumberException;
 import com.italosantos.minha_mesa.exception.NotPermitedException;
+import com.italosantos.minha_mesa.exception.ResourceNotFoundException;
 import com.italosantos.minha_mesa.mapper.TableMapper;
 import com.italosantos.minha_mesa.model.OwnerModel;
 import com.italosantos.minha_mesa.model.TableModel;
@@ -31,5 +32,19 @@ public class TableService {
         TableModel tableModel = this.tableMapper.createToModel(createTableRequestDTO, ownerModel.getRestaurantModel());
 
         return this.tableRepository.save(tableModel);
+    }
+
+    public void deleteTableById(UserModel userModel, Integer id){
+        OwnerModel ownerModel = this.ownerRepository.findByUserModelId(userModel.getId())
+                .orElseThrow(NotPermitedException::new);
+
+        TableModel tableModel = this.tableRepository.findById(id)
+                .orElseThrow(ResourceNotFoundException::new);
+
+        if (! ownerModel.getUserModel().getId().equals(userModel.getId()))
+            throw new NotPermitedException();
+
+        tableModel.setActive(false);
+        this.tableRepository.save(tableModel);
     }
 }
